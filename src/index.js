c2aedc00a333e974d1ccb4bea0b1869829d1e733
Seano <sean01234@yahoo.com>
2023-04-21 17:52:39 -0400

    ramen post update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,56 @@
 // write your code here
 const init=() => {
 
+  //  Listen for form click
+  document.querySelector('#new-ramen').addEventListener('submit',handleSubmit)
+
+  // Event handler
+  let name,restaurant,image,rating,comment
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    let ramenObj={
+      name: e.target.name.value,
+      restaurant: e.target.restaurant.value,
+      image: e.target.image.value,
+      rating: e.target.rating.value,
+      comment: e.target.comment.value
+    }
+    // updated html textarea - comment
+    // console.log(JSON.stringify(ramenObj))
+    renderOneMenuRamen(ramenObj)
+    addRamenToMenu(ramenObj)
+  }
+
+  // POST a ramen - (the Create part of Crud)
+  function addRamenToMenu(ramenObj) {
+    //console.log(JSON.stringify(ramenObj))
+    fetch('http://localhost:3000/ramens',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(ramenObj)
+    })
+      .then(res => res.json())
+      .then(ramen => console.log(ramenObj))
+  }
+
+  // PATCH an ramen - (the UPDATE part of Crud)
+  // Accept request header = content types client can understand
+  function updateDetail(ramenObj) {
+    //console.log(JSON.stringify(ramenObj))
+    fetch(`http://localhost:3000/ramens/${ramenObj.id}`,{
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: "application/json"
+      },
+      body: JSON.stringify(ramenObj)
+    })
+      .then(res => res.json())
+    //.then(ramen => console.log(ramen))
+  }
 
   // renderOneMenuRamen DOM Manipulators
   function renderOneMenuRamen(ramen) {
@@ -10,9 +60,9 @@ const init=() => {
     // innerHTML is coming from us so it should be safe, note backticks
     card.innerHTML=`
       <a class="r-image" id="${ramen.id}}"><img src="${ramen.image}"></a>
-      <div class="content">
+      <!-- <div class="content">
       <h4>${ramen.name}</h4>
-      <p class="r-rating">
+       <p class="r-rating">
         <span class="donation-count">${ramen.rating}</span> Rated
       </p>
       <p class="r-comment">
@@ -20,13 +70,24 @@ const init=() => {
       </p>
       <p class="r-restaurant">
         ${ramen.restaurant}
-      </p>
-      </div>
+      </p> 
+      </div> -->
       `
     // add card to dom
     document.querySelector('div#ramen-menu').appendChild(card)
-  }
 
+    // listener for click on Ramen menu img to update detail
+    card.querySelector('a.r-image').addEventListener('click',() => {
+      //console.log(ramen.name)
+      document.querySelector('img.detail-image').src=ramen.image
+      document.querySelector('h2.name').textContent=ramen.name
+      document.querySelector('h3.restaurant').textContent=ramen.restaurant
+      document.querySelector('span#rating-display').textContent=ramen.rating
+      document.querySelector('p#comment-display').textContent=ramen.comment
+
+      updateDetail(ramen)
+    })
+  }
 
   // Fetch requests
   // GET all ramen resources and render
@@ -35,11 +96,7 @@ const init=() => {
       .then(res => res.json())
       .then(ramenData => ramenData.forEach(ramen => renderOneMenuRamen(ramen)))
   }
-
-
-
   getAllRamens()
-
 }
 
 document.addEventListener("DOMContentLoaded",init);
